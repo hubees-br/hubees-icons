7214f67cfa930b506d381b474a04e54995ef5c58
Mayconn Coelho <mayconn@netpark.com.br>
2022-01-14 10:41:34 -0300

    Prefix on icons

--- rename-files.js
+++ rename-files.js
@@ -2,21 +2,23 @@ const { readdirSync, rename } = require('fs');
 const { resolve } = require('path');
 
 // Get path to image directory
-const imageDirPath = resolve(__dirname, './outline');
-const imageDirPath2 = resolve(__dirname, './solid');
+const outlinePath = resolve(__dirname, './outline');
+const solidPath = resolve(__dirname, './solid');
 
 // Get an array of the files inside the folder
-const files = readdirSync(imageDirPath);
-const files2 = readdirSync(imageDirPath2);
+const outlineFiles = readdirSync(outlinePath);
+const solidFiles = readdirSync(solidPath);
 
 // Loop through each file that was retrieved
-files.forEach((file) => {
-  const newName = 'hubees-' + file.includes('=') ? file.split('=')[1] : file;
-  rename(imageDirPath + `/${file}`, imageDirPath + `/${newName}`, () => {});
+outlineFiles.forEach((outlineFile) => {
+  const formattedOutlineName = outlineFile.includes('=') ? outlineFile.split('=')[1] : outlineFile;
+  const newOutlineName = `hubees-${formattedOutlineName}`;
+  rename(outlinePath + `/${outlineFile}`, outlinePath + `/${newOutlineName}`, () => {});
 });
 
 // Loop through each file that was retrieved
-files2.forEach((file) => {
-  const newName = 'hubees-' + file.includes('=') ? file.split('=')[1] : file;
-  rename(imageDirPath + `/${file}`, imageDirPath + `/${newName}`, () => {});
+solidFiles.forEach((solidFile) => {
+  const formattedSolidName = solidFile.includes('=') ? solidFile.split('=')[1] : solidFile;
+  const newSolidName = `hubees-${formattedSolidName}`;
+  rename(solidPath + `/${solidFile}`, solidPath + `/${newSolidName}`, () => {});
 });
