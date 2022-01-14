a5ed8986069607895a6ff94170c3a2b75fff0c88
Mayconn Coelho <mayconn@netpark.com.br>
2022-01-14 10:19:08 -0300

    Change icon names

---
+++ rename-files.js
@@ -0,0 +1,22 @@
+const { readdirSync, rename } = require('fs');
+const { resolve } = require('path');
+
+// Get path to image directory
+const imageDirPath = resolve(__dirname, './outline');
+const imageDirPath2 = resolve(__dirname, './solid');
+
+// Get an array of the files inside the folder
+const files = readdirSync(imageDirPath);
+const files2 = readdirSync(imageDirPath2);
+
+// Loop through each file that was retrieved
+files.forEach((file) => {
+  const newName = file.includes('=') ? file.split('=')[1] : file;
+  rename(imageDirPath + `/${file}`, imageDirPath + `/${newName}`, () => {});
+});
+
+// Loop through each file that was retrieved
+files2.forEach((file) => {
+  const newName = file.includes('=') ? file.split('=')[1] : file;
+  rename(imageDirPath + `/${file}`, imageDirPath + `/${newName}`, () => {});
+});
