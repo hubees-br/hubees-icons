efe4973f8e5b7116031aeffe9733f0c8406e86a8
Mayconn Coelho <mayconn@netpark.com.br>
2022-02-01 17:27:48 -0300

    Changed lib to work with our project

--- figma.config.js
+++ figma.config.js
@@ -5,41 +5,71 @@ const svgo = require('@figma-export/transform-svg-with-svgo');
 const fileId = process.env.FILE_ID;
 const outputters = [require('@figma-export/output-components-as-svg')({ output: './' })];
 
-/** @type {import('svgo').PluginConfig[]} */
-const solidSVGOConfig = [
-  { name: 'removeDimensions', active: true },
-  { name: 'sortAttrs', active: true },
-  {
-    name: 'removeAttrs',
-    params: {
-      attrs: 'fill',
-    },
-  },
-  {
-    name: 'addAttributesToSVGElement',
-    params: {
-      attribute: { fill: 'currentColor' },
-    },
-  },
-];
+// /** @type {import('svgo').PluginConfig[]} */
+// const solidSVGOConfig = [
+//   { name: 'removeDimensions', active: true },
+//   { name: 'sortAttrs', active: true },
+//   {
+//     name: 'removeAttrs',
+//     params: {
+//       attrs: 'fill',
+//     },
+//   },
+//   {
+//     name: 'addAttributesToSVGElement',
+//     params: {
+//       attribute: { fill: 'currentColor' },
+//     },
+//   },
+// ];
+
+// /** @type {import('svgo').PluginConfig[]} */
+// const outlineSVGOConfig = [
+//   { name: 'removeDimensions', active: true },
+//   { name: 'sortAttrs', active: true },
+//   {
+//     name: 'removeAttrs',
+//     params: {
+//       attrs: 'stroke',
+//     },
+//   },
+//   {
+//     name: 'addAttributesToSVGElement',
+//     params: {
+//       attribute: { stroke: 'currentColor' },
+//     },
+//   },
+// ];
 
 /** @type {import('svgo').PluginConfig[]} */
-const outlineSVGOConfig = [
+const generalSVGOConfig = [
   { name: 'removeDimensions', active: true },
   { name: 'sortAttrs', active: true },
-  {
-    name: 'removeAttrs',
-    params: {
-      attrs: 'stroke',
-    },
-  },
-  {
-    name: 'addAttributesToSVGElement',
-    params: {
-      attribute: { stroke: 'currentColor' },
-    },
-  },
 ];
+
+// /** @type {import('@figma-export/types').FigmaExportRC} */
+// module.exports = {
+//   commands: [
+//     [
+//       'components',
+//       {
+//         fileId,
+//         onlyFromPages: ['solid'],
+//         transformers: [svgo({ multipass: true, plugins: solidSVGOConfig })],
+//         outputters,
+//       },
+//     ],
+//     [
+//       'components',
+//       {
+//         fileId,
+//         onlyFromPages: ['outline'],
+//         transformers: [svgo({ multipass: true, plugins: outlineSVGOConfig })],
+//         outputters,
+//       },
+//     ],
+//   ],
+// };
 
 /** @type {import('@figma-export/types').FigmaExportRC} */
 module.exports = {
@@ -48,17 +78,8 @@ module.exports = {
       'components',
       {
         fileId,
-        onlyFromPages: ['solid'],
-        transformers: [svgo({ multipass: true, plugins: solidSVGOConfig })],
-        outputters,
-      },
-    ],
-    [
-      'components',
-      {
-        fileId,
-        onlyFromPages: ['outline'],
-        transformers: [svgo({ multipass: true, plugins: outlineSVGOConfig })],
+        onlyFromPages: ['Icons'],
+        transformers: [svgo({ multipass: true, plugins: generalSVGOConfig })],
         outputters,
       },
     ],
--- rename-files.js
+++ rename-files.js
@@ -1,24 +1,39 @@
-const { readdirSync, rename } = require('fs');
+const { readdirSync, rename, mkdirSync, existsSync } = require('fs');
 const { resolve } = require('path');
 
 // Get path to image directory
-const outlinePath = resolve(__dirname, './outline');
-const solidPath = resolve(__dirname, './solid');
+// const outlinePath = resolve(__dirname, './outline');
+// const solidPath = resolve(__dirname, './solid');
 
 // Get an array of the files inside the folder
-const outlineFiles = readdirSync(outlinePath);
-const solidFiles = readdirSync(solidPath);
+//const outlineFiles = readdirSync(outlinePath);
+//const solidFiles = readdirSync(solidPath);
 
-// Loop through each file that was retrieved
-outlineFiles.forEach((outlineFile) => {
-  const formattedOutlineName = outlineFile.includes('=') ? outlineFile.split('=')[1] : outlineFile;
-  const newOutlineName = `hubees-${formattedOutlineName}`;
-  rename(outlinePath + `/${outlineFile}`, outlinePath + `/${newOutlineName}`, () => {});
-});
+// // Loop through each file that was retrieved
+// outlineFiles.forEach((outlineFile) => {
+//   const formattedOutlineName = outlineFile.includes('=') ? outlineFile.split('=')[1] : outlineFile;
+//   const newOutlineName = `hubees-${formattedOutlineName}`;
+//   rename(outlinePath + `/${outlineFile}`, outlinePath + `/${newOutlineName}`, () => {});
+// });
 
-// Loop through each file that was retrieved
-solidFiles.forEach((solidFile) => {
-  const formattedSolidName = solidFile.includes('=') ? solidFile.split('=')[1] : solidFile;
-  const newSolidName = `hubees-${formattedSolidName}`;
-  rename(solidPath + `/${solidFile}`, solidPath + `/${newSolidName}`, () => {});
+// // Loop through each file that was retrieved
+// solidFiles.forEach((solidFile) => {
+//   const formattedSolidName = solidFile.includes('=') ? solidFile.split('=')[1] : solidFile;
+//   const newSolidName = `hubees-${formattedSolidName}`;
+//   rename(solidPath + `/${solidFile}`, solidPath + `/${newSolidName}`, () => {});
+// });
+
+const path = resolve(__dirname, './Icons');
+const files = readdirSync(path);
+
+const newPath = resolve(__dirname, './hubees');
+if (!existsSync(newPath)) {
+  mkdirSync(newPath);
+}
+
+files.forEach((file) => {
+  console.log('-');
+  const formattedName = file.includes('=') ? file.split('=')[1] : file;
+  const newName = `${formattedName.toLowerCase()}`;
+  rename(path + `/${file}`, newPath + `/${newName}`, () => {});
 });
